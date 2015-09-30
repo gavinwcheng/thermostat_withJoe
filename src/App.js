6ba67b2b67c8c23539df57eb30d6851561d9397e
chn.challenger <chn.challenger@gmail.com>
2015-09-30 16:50:01 +0100

    working finally

--- src/App.js
+++ src/App.js
@@ -9,8 +9,20 @@ var updateHtmlTemperature = function() {
   document.getElementById("temperature").innerHTML = thermostat.temperature;
 };
 
-
-
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("Up").addEventListener("onclick", userInterate(thermostat.increaseTemperature()));
+  document.getElementById("Up").addEventListener("click", function() {
+    userInterate(thermostat.increaseTemperature());
+  });
+
+  document.getElementById("Down").addEventListener("click", function() {
+    userInterate(thermostat.decreaseTemperature());
+  });
+
+  document.getElementById("Reset").addEventListener("click", function() {
+    userInterate(thermostat.resetTemperature());
+  });
+
+  document.getElementById("PowerSaver").addEventListener("click", function() {
+    userInterate(thermostat.switchPowerSaving());
+  });
 });
